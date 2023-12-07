959ada45cda2151610fd9aefe07f2ea869067ec8
hkolstee <hkolstee@hotmail.com>
2023-12-07 14:01:36 +0100

    some starting changes

---
+++ citylearn-2023-starter-kit/agents/custom_agent.py
@@ -0,0 +1,16 @@
+# the only user agent to the starter kit
+from citylearn.agents.base import Agent
+
+class CustomAgent(Agent):
+    """ Can be any subclass of citylearn.agents.base.Agent """
+    def __init__(self, env):
+        super().__init__(env)
+
+    def register_reset(self, observations):
+        """ Register reset needs the first set of actions after reset """
+        self.reset()
+        return self.predict(observations)
+
+    def predict(self, observations):
+        """ Just a passthrough, can implement any custom logic as needed """
+        return super().predict(observations) 
--- citylearn-2023-starter-kit/agents/user_agent.py
+++ citylearn-2023-starter-kit/agents/user_agent.py
@@ -1,7 +1,8 @@
 from agents.rbc_agent import BasicRBCAgent
+from agents.custom_agent import CustomAgent
 
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
 
-SubmissionAgent = BasicRBCAgent
+SubmissionAgent = CustomAgent
