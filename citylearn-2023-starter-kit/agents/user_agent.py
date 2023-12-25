31712cfca4c8a61e181a9d8b821ce4d0951bd49d
hkolstee <hkolstee@hotmail.com>
2023-12-25 15:00:46 +0100

    more progress in reward function

--- citylearn-2023-starter-kit/agents/user_agent.py
+++ citylearn-2023-starter-kit/agents/user_agent.py
@@ -1,8 +1,7 @@
 from agents.rbc_agent import BasicRBCAgent
-from agents.custom_agent import CustomAgent
 
 ###################################################################
 #####                Specify your agent here                  #####
 ###################################################################
 
-SubmissionAgent = CustomAgent
+SubmissionAgent = BasicRBCAgent
