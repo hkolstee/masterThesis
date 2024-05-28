54f4514d9d355ac845e5cf3ce83d418830541069
hkolstee <hkolstee@hotmail.com>
2024-05-28 15:35:43 +0200

    more runs, better visualization

---
+++ training/spiderenv_training/dqn_3_spider_lowlr/train.py
@@ -0,0 +1,28 @@
+import sys
+from os import path
+
+# append path to import from parent folder
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+
+# custom imports
+from custom_agents.CTCE_algorithms.centralized_DQN import DQN
+
+# custom spider fly environment
+from custom_spider_env.spider_fly_env.envs.grid_MA_pettingzoo2 import SpiderFlyEnvMA
+from custom_spider_env.spider_fly_env.wrappers.pettingzoo_wrapper import PettingZooWrapper
+
+def main():
+    env = SpiderFlyEnvMA(size = 4, spiders = 3, max_timesteps = 100)
+
+    # pettingzoo conversion + normalization wrappper
+    env = PettingZooWrapper(env, normalize = True)
+    
+    # create model
+    seqdqn = DQN(1e-4, env, batch_size = 256, layer_sizes = (128, 128), eps_steps = 1000000, global_observations = True, buffer_max_size = 1000000, tau = 0.0025) 
+    
+    # train agent (ep = 720)
+    seqdqn.train(num_episodes = 30000000)
+
+if __name__ == "__main__":
+    main()
+    
